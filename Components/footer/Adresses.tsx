5fca88d42bbaef054cbbd204476a2c37a858880d
amineouabbou <amineouabbou@gmail.com>
2022-12-08 10:59:52 +0100

    initial2

--- Components/footer/Adresses.tsx
+++ Components/footer/Adresses.tsx
@@ -5,7 +5,7 @@ const Adresses = () => {
   return (
     <div>
       <Bloc
-        title="Adresse GENÈVE"
+        title="Adresse GENÈVE "
         desc="Lorem ipsum <br> CH - 1205 GENÈVE<br> Tél : +41 20 600 00 00"
       />
 
